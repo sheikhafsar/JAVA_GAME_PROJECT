f622dd47b403cf3df09eec95563da9912b663081
sheikhafsar <sheikhafsar72@gmail.com>
2018-11-16 23:50:17 +0530

    racquet class

--- src/miniTennis1/Game.java
+++ src/miniTennis1/Game.java
@@ -4,19 +4,44 @@ package miniTennis1;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
 
 
 public class Game extends JPanel{
 	
 	Ball ball = new Ball(this);
-
-	private void moveBall() {
-		ball.move();
-	}
+	Racquet racquet = new Racquet(this);
 	
+	public Game() {
+		addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				racquet.keyReleased(e);
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				racquet.keyPressed(e);
+			}
+		});
+		setFocusable(true);
+	}
+
+	private void move() {
+		ball.move();
+		racquet.move();
+	}
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
@@ -24,24 +49,31 @@ public class Game extends JPanel{
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
 		ball.paint(g2d);
+		racquet.paint(g2d);
+	}
+	
+	public void gameOver() {
+		JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+		System.exit(ABORT);
 	}
 
+	
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		
 		JFrame frame = new JFrame("Mini Tennis");
-		
 		Game game = new Game();
-		
 		frame.add(game);
-		frame.setSize(300, 300);
+		frame.setSize(300, 400);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		while (true) {
-			game.moveBall();
+			game.move();
 			game.repaint();
 			Thread.sleep(10);
 		}
+
 	}
 
 }
